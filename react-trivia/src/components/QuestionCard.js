b4cf8d9076d2dcd500f8bea90b1ddfbb5498b171
TrentHurlbut <trent.hurlbut@gmail.com>
2021-12-08 09:41:57 -0800

    Game more or less functions. Needs more styling.

--- react-trivia/src/components/QuestionCard.js
+++ react-trivia/src/components/QuestionCard.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
+
 export default function QuestionCard({ clickFunction, text, correctAnswer, answers }) {
-  
-  answers.push(correctAnswer);
+
+  const[answerStatus, setAnswerStatus] = useState(null)
 
   console.log(answers)
+  console.log(correctAnswer)
 
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -11,22 +14,74 @@ export default function QuestionCard({ clickFunction, text, correctAnswer, answe
     }
   };
 
-  shuffleArray(answers);
+  let newAnswers = [correctAnswer, ...answers];
+  shuffleArray(newAnswers);
+  console.log(`question card renders ${text}`)
 
-  return (
+  return (answerStatus === null ? (
     <div className="question-card">
-      <section className="nes-container is-dark">
+        <section className="nes-container is-dark">
+          <section className="message-list">
+            <section className="message -left">
+              <i className="nes-bcrikko"></i>
+              <div className="nes-balloon from-left is-dark">
+                <p>{text}</p>
+              </div>
+            </section>
+          </section>
+        </section>
+      {newAnswers.map((answer) => (<button
+        type="button"
+        className="nes-btn is-warning ab"
+        onClick={() => {
+          if (answer === correctAnswer) {
+              setAnswerStatus('correct')
+          } else {
+              setAnswerStatus('incorrect')
+          }
+        }}>{answer}</button>))}
+        <button type="button" className="nes-btn is-primary qb" onClick={() => { clickFunction() }}>Back</button>
+    </div>
+  )
+    :
+    answerStatus === 'correct' ? (
+      <div className="questions-screen">
+        <section className="nes-container is-dark">
         <section className="message-list">
           <section className="message -left">
             <i className="nes-bcrikko"></i>
             <div className="nes-balloon from-left is-dark">
-              <p>{text}</p>
+              <p>Correct! Great job!</p>
             </div>
           </section>
         </section>
+        </section>
+        <i class="nes-icon coin is-large"></i>
+        <i class="nes-icon coin is-large"></i>
+        <i class="nes-icon coin is-large"></i>
+        <i class="nes-icon coin is-large"></i>
+        <i class="nes-icon coin is-large"></i>
+        <i class="nes-icon coin is-large"></i>
+      </div>
+    ) : (
+    <div className="questions-screen">
+      <section className="nes-container is-dark">
+      <section className="message-list">
+        <section className="message -left">
+          <i className="nes-bcrikko"></i>
+          <div className="nes-balloon from-left is-dark">
+            <p>Oooohhhh... Sorry! Wrong Answer!</p>
+          </div>
+        </section>
       </section>
-      {answers.map((answer) => (<button type="button" className="nes-btn is-warning ab">{answer}</button>))}
-      <button type="button" className="nes-btn is-primary qb" onClick={() => { clickFunction() }}>Back</button>
+      </section>
+      <i class="nes-icon close is-large"></i>
+      <i class="nes-icon close is-large"></i>
+      <i class="nes-icon close is-large"></i>
+      <i class="nes-icon close is-large"></i>
+      <i class="nes-icon close is-large"></i>
+      <i class="nes-icon close is-large"></i>
     </div>
+  )
   )
 }
--- react-trivia/src/components/QuestionsScreen.js
+++ react-trivia/src/components/QuestionsScreen.js
@@ -4,18 +4,22 @@ import QuestionCard from './QuestionCard';
 
 export default function QuestionsScreen({ category, clickFunction }) {
 
-  const[questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState([])
 
   useEffect(() => {
     axios.get(`https://opentdb.com/api.php?amount=10&category=${category}&type=multiple`)
       .then(response => {
         setQuestions(response.data.results)
-        })
+      })
   }, [])
-  
+
   return (
     <div className="questions-screen">
-      {questions.map((question) => (<QuestionCard text={question.question} clickFunction={clickFunction} correctAnswer={question.correct_answer} answers={question.incorrect_answers} />))}
-    </div>
-  )
+      {questions.map((question) => (<QuestionCard
+        text={question.question}
+        clickFunction={clickFunction}
+        correctAnswer={question.correct_answer}
+        answers={question.incorrect_answers}
+      />))}
+    </div>)
 }
